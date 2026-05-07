79755d57d919ad7422d1bbe92520853ecd9a9041
Francisco Javier García Cañero <franciscoj.garciacan@gmail.com>
2026-05-07 14:14:25 +0200

    feat: añadir avatar con iniciales y nombre de empresa en header del shell

--- src/main/java/com/gestorrh/escritorio/presentation/controller/ShellController.java
+++ src/main/java/com/gestorrh/escritorio/presentation/controller/ShellController.java
@@ -17,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
